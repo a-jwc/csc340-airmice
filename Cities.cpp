f023d65077048418e882b88fc351bc3369140a2e
alexjwchau-sfsu <achau3@mail.sfsu.edu>
2020-10-26 13:58:24 -0700

    created Citiescpp and Citiesh

---
+++ Cities.cpp
@@ -0,0 +1,4 @@
+#include  <iostream>
+#include "Cities.h"
+
+using namespace std;
---
+++ Cities.h
@@ -0,0 +1,16 @@
+#ifndef CITIES_H
+#define CITIES_H
+
+#include <vector>
+#include "City.h"
+
+using namespace std;
+
+class City {
+    public:
+        
+    private:
+        vector<City> cityList;
+};
+
+#endif
